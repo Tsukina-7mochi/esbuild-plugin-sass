727ee382d1bb4b8b0f23a4bc47ea8b2ffa52761f
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-21 23:14:12 +0900

    docs: add example

---
+++ example/build.ts
@@ -0,0 +1,18 @@
+import * as esbuild from 'esbuild';
+import sassPlugin from '../mod.ts';
+
+await Promise.all([
+  esbuild.build({
+    entryPoints: ['./style.scss'],
+    bundle: true,
+    outfile: './style.css',
+    plugins: [sassPlugin()],
+  }),
+  esbuild.build({
+    entryPoints: ['./style.scss'],
+    bundle: true,
+    outfile: './style.min.css',
+    plugins: [sassPlugin()],
+    minify: true,
+  }),
+]);
