e7d84df72f8bc574afdb925321fa9ac13ed7f56b
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-21 23:12:58 +0900

    feat: implement core functions

---
+++ mod.ts
@@ -0,0 +1,42 @@
+import * as path from '@std/path';
+
+import type * as esbuild from 'esbuild';
+import * as sass from 'sass';
+
+interface SassPluginOptions {
+  loader?: esbuild.Loader;
+  sassOptions?: sass.Options<'async'>;
+}
+
+const sassPlugin = (options: SassPluginOptions = {}): esbuild.Plugin => {
+  return {
+    name: 'esbuild-plugin-sass',
+    setup: (build) => {
+      const buildOptions = build.initialOptions;
+      const sassOptions = {
+        style: buildOptions.minify
+          ? 'compressed' as const
+          : 'expanded' as const,
+        sourceMap: buildOptions.sourcemap !== false &&
+          buildOptions.sourcemap !== undefined,
+        ...(options.sassOptions ?? {}),
+      };
+
+      build.onLoad({ filter: /\.s[ac]ss/ }, async (args) => {
+        const result = await sass.compileAsync(args.path, sassOptions);
+        const watchFiles = result.loadedUrls
+          .filter((url) => url.protocol === 'file:')
+          .map((url) => path.fromFileUrl(url));
+        // TODO: support source maps
+
+        return {
+          contents: result.css,
+          loader: options.loader ?? 'css',
+          watchFiles,
+        };
+      });
+    },
+  };
+};
+
+export default sassPlugin;
